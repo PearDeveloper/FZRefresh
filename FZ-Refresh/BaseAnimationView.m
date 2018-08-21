9a29e71187be0630942400f5d1d3f1c3d1b74283
王欣 <573385822@qq.com>
2018-08-21 18:11:08 +0800

    第一次提交

---
+++ FZ-Refresh/BaseAnimationView.m
@@ -0,0 +1,38 @@
+//
+//  BaseAnimationView.m
+//  PullToRefresh
+//
+//  Created by 王欣 on 2018/8/20.
+//  Copyright © 2018年 王欣. All rights reserved.
+//
+
+#import "BaseAnimationView.h"
+
+@implementation BaseAnimationView
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self prepare];
+        [self setupSublayers];
+    }return self;
+}
+-(void)prepare{
+    self.backgroundColor = [UIColor clearColor];
+}
+-(void)setupSublayers{
+    
+}
+//更新进度
+-(void)updateWithProgress:(CGFloat)progress isHeader:(BOOL)header{
+    
+}
+//开始动画
+-(void)startAnimation{
+    
+}
+
+//结束动画
+-(void)stopAnimaiton{
+    
+}
+
+@end
